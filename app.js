5d06af6b93b816e7b935a00353edb54783ff21fb
AMAN <antilaman3113@gmail.com>
2022-09-16 21:04:36 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const hexEl = document.querySelector("#color-picker");
 const modeEl = document.querySelector("#color-type");
-modeEl.addEventListener("input", function () {
-  console.log(modeEl.Value);
-});
+const colorDisplay = document.querySelector(".color-display");
+const hexFooter = document.querySelector(".hex-footer");
+const submitBtn = document.querySelector("#getColor-btn");
 
-// fetch(" https://www.thecolorapi.com/scheme?")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
+submitBtn.addEventListener("click", function () {
+  let modeValue = modeEl.options[modeEl.selectedIndex].value.toLowerCase();
+  let hexValue = hexEl.value.slice(1);
+  let url = `
+  https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${modeValue}&count=5
+  `;
+  console.log(url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      let colorArray = data.colors;
+      let html = "";
+      let code = "";
+      for (let i = 0; i < colorArray.length; i++) {
+        html += `
+       <div class="color-image" style="background-color : ${colorArray[i].hex.value}">
+       </div>
+       `;
+        code += `
+       <div class="hex-code">
+       ${colorArray[i].hex.value}
+       </div>
+       `;
+      }
+      colorDisplay.innerHTML = html;
+      hexFooter.innerHTML = code;
+    });
+});
